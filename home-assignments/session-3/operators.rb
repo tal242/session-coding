d8816660c721f9b4700df8898e978f55587f021c
Eyal Stoler <eyal.stoler@kenshoo.com>
2016-10-06 17:50:51 +0300

    Create operators.rb

---
+++ home-assignments/session-3/operators.rb
@@ -0,0 +1,10 @@
+#fill in the boolean values
+
+# boolean_1 = 77 < 78 && 77 < 77
+boolean_1 = 
+
+# boolean_2 = true && 100 >= 100
+boolean_2 = 
+
+# boolean_3 = 2**3 == 8 && 3**2 == 9
+boolean_3 = 
