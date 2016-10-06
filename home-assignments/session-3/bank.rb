77cfdf7552314ed8dfc0e149861d2a4b8b11c94c
Eyal Stoler <eyal.stoler@kenshoo.com>
2016-10-06 17:49:24 +0300

    Create bank.rb

---
+++ home-assignments/session-3/bank.rb
@@ -0,0 +1,42 @@
+# print_action method expects two parameters "action" and "amount"
+def print_action(action, amount)
+  puts "You've asked to #{action} an amount of #{amount}$. Performing your request..."
+end
+
+balance = 1000
+
+print "Please enter your personal name:  "
+name = gets.chomp.capitalize!
+
+print "What action would you like to perform?  "
+action = gets.chomp
+
+amount = 20
+
+print_action(amount)
+
+if action == "withdraw"
+  balance -= amount
+elsif action == "deposit"
+  balance += amount
+
+
+puts "Hi #{name}, Your current balance was changed and is now: #{balance}$"  
+
+
+# Bank home assignment
+# ---------------------
+
+# The above code imitates a simple withdrawal and deposit from a bank account with some credit balance.
+
+# Please first go over each line and make sure you understand what each line does if not. Please go back to the class lecture and review it.
+
+# There are some things you need to do with this code:
+
+#   1. Run it! If it’s working, great! If it’s not, well, find out what is missing/wrong and fix it so it will run. (Continue to next missions only after the code is running)
+#   2. As you can see the two available actions are “withdraw” & “deposit”. However, what if I’m only interested in viewing my account credit? Please add a third option called “view” that only prints out to the screen the current credit (the “amount” variable).
+#   3. You can also notice that the actual “amount” variable is always set to a fixed number 20 (line 14). Let’s change it to a user input (like “action” and “name” variables do).
+#   4. The “balance” variable (line 6) is also hard coded to 100. Find a way to always set it to a random number between 100 and 1000 when the program starts (Hint: Ruby already knows how to do this you don’t need to implement such a functionality).
+#   5. Okay. now pay attention to the "balance" variable. There is no way to know what was the initial balance before the change. Please also print what was the previous balance in every place that you change it ("withdraw" and "deposit")
+#   6. What happens when you input an unknown “action” like “stop” or “blabla”? I think this is unclear. Let’s raise an exception when an unclear action is being set as the “action” by the user input.
+#   7. And finally a question: Why did I use the “print_action” method in this code? What principle out of the “coding practices for beginners” we’ve learned (see lecture notes) does it serve? Write the answer as a comment in the code.
