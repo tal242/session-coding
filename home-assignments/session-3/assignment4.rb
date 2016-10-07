1479ed82648b74c32283b42ae6cbf6eb8857f545
david abutbul <david@abutbul.com>
2016-10-07 11:26:57 +0300

    after assignment 5

--- home-assignments/session-3/assignment4.rb
+++ home-assignments/session-3/assignment4.rb
@@ -6,13 +6,12 @@ print "Please enter your name:"
 name = gets.chomp.capitalize!
 print "Please enter your age:"
 age = gets.chomp.to_i
+if age >= 130
+  raise "Illegal Age are you alien?"
+end
 until age.%(7).zero?
   age = (age += 1)
 end
-#until age % 7 == 0 
-#  age += 1
-#end
 
 dogyears = dogify(age)
 puts "Hello #{name}, your age is #{dogyears} in dog years"
-
