3091df0cf3359b06674587a6d918a7ee25460783
adir <adir.s1@gmail.com>
2016-10-15 14:48:09 -0700

    updated fix-loop.rb operators.rb

--- home-assignments/session-3/operators.rb
+++ home-assignments/session-3/operators.rb
@@ -1,10 +1,10 @@
 #fill in the boolean values
 
 # boolean_1 = 77 < 78 && 77 < 77
-boolean_1 = 
+boolean_1 = false
 
 # boolean_2 = true && 100 >= 100
-boolean_2 = 
+boolean_2 = true
 
 # boolean_3 = 2**3 == 8 && 3**2 == 9
-boolean_3 = 
+boolean_3 = true
