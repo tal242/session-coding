28db78211d3c3fce54930aad75b1a6301d72d059
Avital Zaltzman <talz1010@gmail.com>
2016-11-01 10:18:24 -0700

    Fixed

--- home-assignments/session-4/animals.rb
+++ home-assignments/session-4/animals.rb
@@ -1,30 +1,39 @@
-begin
 
-class animal
+class Animal
 attr_accessor :name
 attr_accessor :color
 
 def initialize(name, color)
  @name = name
  @color = color
-end
-end
-
-animal_list=animal.new({"rabbit":"gray", "mouse":"gray", "deer":"brown", "bear":"brown", "sheep":"white", "flamingo":"pink", "beetle":"red", "duck":"white", "ant": "black", "donkey":"brown", "dolphin":"gray"})
-
-puts "List of animals with number of characters in their name smaller than 5" 
-
-if animal.name.length < 5
-
+ end
 end
 
-puts "List of animals with the color “brown"
-puts animal_list.select {|key,value| value==brown}
+animals_list=[Animal.new("rabbit","gray"),
+	 Animal.new("mouse","gray"),
+	 Animal.new("deer","brown"), 
+	 Animal.new("bear","brown"),
+	 Animal.new("sheep","white"),
+	 Animal.new("flamingo","pink"),
+ 	 Animal.new("beetle","red"),
+	 Animal.new("duck","white"),
+	 Animal.new("ant","black"),
+	 Animal.new("donkey","brown"),
+	 Animal.new("dolphin","gray")]
 
-end
+puts "List of animals with number of characters in their name smaller than 5"
+animals_list.each do |animal|
+  puts  "#{animal.name}" if animal.name.length < 5
+   end
 
-puts "Animals  color is gray"
-puts animal_list.fetch("gray") {|animal| "Wow!" }
+puts "List of animals with the color brown"
+    animals_list.each do |animal|
+    puts  "#{animal.name}" if animal.color=="brown"
+    end
+
+    puts "Animals  color is gray"
+    animals_list.each do |animal|
+    puts "Wow!" if animal.color=="gray"
+    end
 
 
-end
